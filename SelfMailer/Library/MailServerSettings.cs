3adf6801bf3f04655cada9acfa5895ae91baee67
CC <cchiglien@icloud.com>
2016-11-15 11:45:45 +0100

    test

---
+++ SelfMailer/Library/MailServerSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SelfMailer.Library
+{
+    public class MailServerSettings : IReportChange
+    {
+        //ii
+        protected bool hasChanged;
+
+        public bool HasChanged
+        {
+            get { return hasChanged; }
+            set
+            {
+                if (this.hasChanged != value)
+                {
+                    this.hasChanged = value;
+                    if (this.Changed != null)
+                        this.Changed(this, new EventArgs());
+                }
+            }
+        }
+
+        public event EventHandler Changed;
+    }
+}
